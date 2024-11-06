7641f037165fe11699488c937e2d926638def276
frippiat <161236309+frippiat@users.noreply.github.com>
2024-11-06 19:57:47 +0100

    Update vertexShader.glsl

--- src/vertexShader.glsl
+++ src/vertexShader.glsl
@@ -15,7 +15,7 @@ void main()
     vec4 worldPosition = modelMatrix * vec4(vPosition, 1.0); //World position in 3D space of the planet
     fPosition = vec3(worldPosition); 
     fNormal =mat3(transpose(inverse(modelMatrix))) * vNormal;  // Normals must follow the planet after their transformation
-    gl_Position = projMat * viewMat * worldPosition;
+    gl_Position = projMat * viewMat * worldPosition; //this is done to rasterize: rasterization is the process of converting 3D geometric data (like vertices and shapes) into a 2D pixel-based image
     fTexCoord=vTexCoord;
 }
 
